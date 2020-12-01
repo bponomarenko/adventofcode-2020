81c689024ce5f2b36c903a10871af12df08e46f5
Borys Ponomarenko <bponomarenko@gmail.com>
2020-12-01 23:46:40 +0100

    initial setup

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+#[path = "task_01_1.rs"]
+mod task;
+
+fn main() {
+    task::run(&read_input());
+}
+
+fn read_input() -> String {
+    println!("Reading input.txt");
+    let path = env::current_dir()
+        .expect("Cannot get current directory")
+        .join(Path::new("input.txt"));
+    fs::read_to_string(path).expect("Unable to read input.txt file")
+}
---
+++ src/task_01_1.rs
@@ -0,0 +1,21 @@
+pub fn run(input: &String) {
+    let entries: Vec<u32> = input
+        .split_whitespace()
+        .map(|s| s.parse().expect("Input should consist of numbers"))
+        .collect();
+
+    println!("Result: {}", find_matching_entries(&entries));
+}
+
+fn find_matching_entries(entries: &Vec<u32>) -> u32 {
+    for (i, entry) in entries.iter().enumerate() {
+        for (j, sub_entry) in entries[(i + 1)..entries.len()].iter().enumerate() {
+            for sub_sub_entry in entries[(j + 1)..entries.len()].iter() {
+                if entry + sub_entry + sub_sub_entry == 2020 {
+                    return entry * sub_entry * sub_sub_entry;
+                }
+            }
+        }
+    }
+    return 0;
+}
