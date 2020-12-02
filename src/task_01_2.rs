bcf15054fdd415694e2b4f728ae5e39cea34b8c1
Borys Ponomarenko <bponomarenko@gmail.com>
2020-12-02 09:57:38 +0100

    Add second task

--- src/task_01_2.rs
+++ src/task_01_2.rs
@@ -9,9 +9,11 @@ pub fn run(input: &String) {
 
 fn find_matching_entries(entries: &Vec<u32>) -> u32 {
     for (i, entry) in entries.iter().enumerate() {
-        for sub_entry in entries[(i + 1)..entries.len()].iter() {
-            if entry + sub_entry == 2020 {
-                return entry * sub_entry;
+        for (j, sub_entry) in entries[(i + 1)..entries.len()].iter().enumerate() {
+            for sub_sub_entry in entries[(j + 1)..entries.len()].iter() {
+                if entry + sub_entry + sub_sub_entry == 2020 {
+                    return entry * sub_entry * sub_sub_entry;
+                }
             }
         }
     }
