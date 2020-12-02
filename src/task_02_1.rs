4bb71a59aff473c22fb91957d6a653cb7cb415ee
Borys Ponomarenko <bponomarenko@gmail.com>
2020-12-02 13:53:53 +0100

    fixes

--- src/task_02_1.rs
+++ src/task_02_1.rs
@@ -1,11 +1,14 @@
 pub fn run(input: &String) {
     let count = input
         .split_terminator('\n')
+        // Filter only strings with passwords, which satisfy requirements
         .filter(|s| {
+            // Split into [min-max requirements, char, password]
             let partials: Vec<&str> = s.split_whitespace().collect();
             let char = partials[1].replace(":", "");
+            // Calculate how many times "char" is present in the password word
             let matches_count = partials[2].matches(&char).count() as u8;
-
+            // Parse "min-max" into [min, max]
             let min_max: Vec<u8> = partials[0]
                 .split('-')
                 .map(|s| s.parse::<u8>().expect("Min-max limit should be defined with numbers"))
--- src/task_02_2.rs
+++ src/task_02_2.rs
@@ -1,17 +1,21 @@
 pub fn run(input: &String) {
     let count = input
         .split_terminator('\n')
+        // Filter only strings with passwords, which satisfy requirements
         .filter(|s| {
+            // Split into [min-max requirements, char, password]
             let partials: Vec<&str> = s.split_whitespace().collect();
             let char = partials[1].chars().nth(0).unwrap();
+            // Parse "min-max" into [min, max]
             let min_max: Vec<u8> = partials[0]
                 .split('-')
                 .map(|s| s.parse::<u8>().expect("Min-max limit should be defined with numbers"))
                 .collect();
 
             let min_char = partials[2].chars().nth(min_max[0] as usize - 1).unwrap();
-            let max_char = partials[2].chars().nth(min_max[0] as usize - 1).unwrap();
-            char.eq(&min_char) | char.eq(&max_char)
+            let max_char = partials[2].chars().nth(min_max[1] as usize - 1).unwrap();
+            // Use XOR to determine when only one of two chars is matching
+            min_char.eq(&char) ^ max_char.eq(&char)
         })
         .count();
 
