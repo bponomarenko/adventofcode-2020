b4bfe39cc4000238c15aa3a5a836a134de6f312d
Borys Ponomarenko <bponomarenko@gmail.com>
2020-12-05 10:13:11 +0100

    Day 5 part 1

---
+++ src/task_05_1.rs
@@ -0,0 +1,18 @@
+pub fn run(input: &String) {
+    println!("Result: {}", input.split_whitespace().map(|s| parse_id(s)).max().unwrap());
+}
+
+fn parse_id(line: &str) -> u32 {
+    let bin = line.replace("F", "0").replace("L", "0").replace("B", "1").replace("R", "1");
+    isize::from_str_radix(&bin, 2).unwrap() as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_id;
+
+    #[test]
+    fn should_convert_binary_to_int() {
+        assert_eq!(parse_id("FBFBBFFRLR"), 357);
+    }
+}
